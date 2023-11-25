732f0977bc74e1d30242b4979fe9ca5b7a266f12
Alexeykedr <i@2kulakov.ru>
2023-11-25 09:32:57 +0300

    Initial commit

---
+++ src/main/java/org/example/repository/jdbcimpl/JdbcDeveloperRepositoryImpl.java
@@ -0,0 +1,33 @@
+package org.example.repository.impl;
+
+import org.example.model.Developer;
+import org.example.repository.DeveloperRepository;
+
+import java.util.List;
+
+public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
+    @Override
+    public Developer create(Developer developer) {
+        return null;
+    }
+
+    @Override
+    public Developer get(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public List<Developer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Developer update(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+}
---
+++ src/main/java/org/example/repository/jdbcimpl/JdbcSkillRepositoryImpl.java
@@ -0,0 +1,33 @@
+package org.example.repository.impl;
+
+import org.example.model.Skill;
+import org.example.repository.SkillRepository;
+
+import java.util.List;
+
+public class JdbcSkillRepositoryImpl implements SkillRepository {
+    @Override
+    public Skill create(Skill skill) {
+        return null;
+    }
+
+    @Override
+    public Skill get(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public List<Skill> getAll() {
+        return null;
+    }
+
+    @Override
+    public Skill update(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+}
