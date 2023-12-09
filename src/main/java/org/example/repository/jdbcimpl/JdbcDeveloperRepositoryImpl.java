280fe9e5f49c2ff7fa991c9d52b3791159eede14
Alexeykedr <i@2kulakov.ru>
2023-12-09 10:47:50 +0300

    version  0.1

--- src/main/java/org/example/repository/jdbcimpl/JdbcDeveloperRepositoryImpl.java
+++ src/main/java/org/example/repository/jdbcimpl/JdbcDeveloperRepositoryImpl.java
@@ -14,8 +14,21 @@ import java.util.List;
 
 public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
     private final String CREATE_DEVELOPER = "insert into developers(first_name, last_name) values (?,?)";
-    private final String GET_DEVELOPER_BY_ID = " SELECT d.id, d.first_name, d.last_name, d.status, " + "s.name as skill_name, sp.name as specialty_name " + "FROM developers d " + "LEFT JOIN developer_skills ds ON d.id = ds.developer_id " + "LEFT JOIN skills s ON ds.skill_id = s.id " + "LEFT JOIN specialties sp ON d.specialty_id = sp.id; ";
-    private final String GET_ALL_DEVELOPERS = " SELECT d.id, d.first_name, d.last_name, d.status, " + "s.name as skill_name, sp.name as specialty_name " + "FROM developers d " + "LEFT JOIN developer_skills ds ON d.id = ds.developer_id " + "LEFT JOIN skills s ON ds.skill_id = s.id " + "LEFT JOIN specialties sp ON d.specialty_id = sp.id; ";
+    private final String GET_DEVELOPER_BY_ID =
+            "SELECT d.id, d.first_name, d.last_name,d.specialty" +
+                    "s.name as skill_name" +
+                    "FROM developers d" +
+                    "LEFT JOIN developer_skills ds ON d.id = ds.developer_id" +
+                    "LEFT JOIN skills s ON ds.skill_id = s.id" +
+                    "LEFT JOIN specialties sp ON sp.name = d.specialty" +
+                    "where d.id = ?";
+    private final String GET_ALL_DEVELOPERS =
+            "SELECT d.id, d.first_name, d.last_name,d.specialty" +
+                    "s.name as skill_name" +
+                    "FROM developers d" +
+                    "LEFT JOIN developer_skills ds ON d.id = ds.developer_id" +
+                    "LEFT JOIN skills s ON ds.skill_id = s.id" +
+                    "LEFT JOIN specialties sp ON sp.name = d.specialty";
     private final String UPDATE_DEVELOPER_BY_ID = "update developers set (first_name, last_name) values (?,?) where id ?";
     private final String DELETE_DEVELOPER_BY_ID = "delete from developers where id = ?";
 
@@ -29,7 +42,6 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
     @Override
     public Developer create(Developer developer) {
         try (PreparedStatement preparedStatement =
-//                     DBUtils.getPreparedStatement(CREATE_DEVELOPER)) {
                      DBUtils.getPreparedStatementWithKeys(CREATE_DEVELOPER)) {
             preparedStatement.setString(1, developer.getFirstName());
             preparedStatement.setString(2, developer.getLastName());
@@ -55,7 +67,7 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
     public Developer get(Long id) {
         try (PreparedStatement preparedStatement = DBUtils.getPreparedStatement(GET_DEVELOPER_BY_ID)) {
             preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            ResultSet resultSet = preparedStatement.executeQuery(); //здесь не отрабатывает
             return mapResultSettoDeveloper(resultSet);
         } catch (SQLException e) {
             return null;
@@ -78,7 +90,7 @@ public class JdbcDeveloperRepositoryImpl implements DeveloperRepository {
                 Skill skill = new Skill(skillId, skillName);
                 List<Skill> skills = listDevelopers.get(developerId.intValue()).getSkills();
                 //SPECIALTY
-                Long specialtyId = resultSet.getLong("specialty_id");
+                Long specialtyId = resultSet.getLong("id");
                 String specialtyName = resultSet.getString("specialty_name");
                 Specialty specialty = new Specialty(specialtyId, specialtyName);
 
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS specialties
 CREATE TABLE IF NOT EXISTS developers
 (
     id           BIGSERIAL PRIMARY KEY,
-    first_name   VARCHAR(64)  NOT NULL,
+    first_name   VARCHAR(128)  NOT NULL,
     last_name    VARCHAR(128) NOT NULL,
-    status       VARCHAR(64)
+    specialty VARCHAR(128)
     );
 
 CREATE TABLE IF NOT EXISTS skills
@@ -20,19 +20,24 @@ CREATE TABLE IF NOT EXISTS skills
 
 CREATE TABLE IF NOT EXISTS developer_skills
 (
-developer_id BIGSERIAL PRIMARY KEY,
+    developer_id BIGSERIAL PRIMARY KEY,
     skill_id BIGSERIAL,
 
-FOREIGN KEY (developer_id) REFERENCES developers(id),
+    FOREIGN KEY (developer_id) REFERENCES developers(id),
     FOREIGN KEY (skill_id) REFERENCES skills(id),
     UNIQUE (developer_id,skill_id)
-);
-
-
-
+    );
 
 INSERT INTO specialties (name)
 VALUES ('developer');
 
-INSERT INTO developers (first_name, last_name)
-VALUES ('Petr', 'Petrov');
+INSERT INTO developers (first_name, last_name, specialty)
+VALUES ('Petr', 'Petrov', 'java');
+
+
+SELECT d.id, d.first_name, d.last_name,d.specialty,
+       s.name as skill_name
+FROM developers d
+         LEFT JOIN developer_skills ds ON d.id = ds.developer_id
+         LEFT JOIN skills s ON ds.skill_id = s.id
+         LEFT JOIN specialties sp ON sp.name = d.specialty;
